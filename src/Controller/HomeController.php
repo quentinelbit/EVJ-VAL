298568419b910b45174a17184a5ad020a1b910fe
quentinelbit <quentinp.elbitmedia@gmail.com>
2023-02-16 16:58:59 +0100

    Timeo

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -15,30 +15,4 @@ class HomeController extends AbstractController
             'controller_name' => 'HomeController',
         ]);
     }
-
-    #[Route('/timeo', name: 'app_timeo')]
-    public function timeo(): Response
-    {
-        return $this->render('home/timeo.html.twig', [
-            'controller_name' => 'HomeController',
-        ]);
-    }
-
-    #[Route('/timeo/hogwart', name: 'app_timeo_hogwart')]
-    public function timeo_hogwart(): Response
-    {
-        return $this->render('home/timeo_hogwart.html.twig', [
-            'controller_name' => 'HomeController',
-        ]);
-    }
-
-    #[Route('/timeo/rocket', name: 'app_timeo_rocket')]
-    public function timeo_rocket(): Response
-    {
-        return $this->render('home/timeo_rocket.html.twig', [
-            'controller_name' => 'HomeController',
-        ]);
-    }
-
-
 }
